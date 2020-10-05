3c9096ce5e21ae93a627612cd582be7bb09957f1
andreas <andreas@andreas-thinkpad.fritz.box>
2020-10-05 20:56:29 +0200

    initial commit comment

---
+++ src/test/java/net/vpein/graylogSimple/GraylogSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vpein.graylogSimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraylogSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
